916e2397fd2b9eb2846bc22e3c405071577b51bf
Viktor <viktor.sbruev.1999@gmail.com>
2025-12-24 19:30:16 +0200

    Added task CW_8_25

---
+++ level_8/CW_8_25/CW_8_25.go
@@ -0,0 +1,13 @@
+/*
+Write a function that converts the given input string to uppercase.
+
+Напишите функцию, которая преобразует входную строку в верхний регистр.
+*/
+
+package main
+
+import "strings"
+
+func MakeUpperCase(input string) string {
+	return strings.ToUpper(input)
+}
